eec9676d184b69db293f87e935d0a87aa91e7ed6
John Bell <insidmal@insidmal.com>
2017-11-01 15:51:57 -0700

    azure practice stuff

--- Lab 5/BookCatalog/BookCatalog/Pages/Index.cshtml.cs
+++ Lab 5/BookCatalog/BookCatalog/Pages/Index.cshtml.cs
@@ -4,14 +4,29 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
 
 namespace BookCatalog.Pages
 {
     public class IndexModel : PageModel
     {
+        private string text;
+        private string name;
+        public string Text { get { return text; } set { text = value; } }
+        [Display(Name = "Your Name")]
+        public string Name { get { return name; } set { name = value; } }
+
+
         public void OnGet()
         {
 
+        }
+
+        public IActionResult OnPost()
+        {
+
+            text = "Thanks, now <a href=\"Books/Index\">click here to view the list</a>.";
+            return Page();
         }
     }
 }
